368ed8d5a010893649c926f0645c1139e40fc35b
Your Name <williamputnamjr@gmail.com>
2017-10-17 15:45:17 -0700

    added .last function

--- src/underbar.js
+++ src/underbar.js
@@ -7,6 +7,7 @@
   // seem very useful, but remember it--if a function needs to provide an
   // iterator when the user does not pass one in, this will be handy.
   _.identity = function(val) {
+    return val;
   };
 
   /**
@@ -37,6 +38,15 @@
   // Like first, but for the last elements. If n is undefined, return just the
   // last element.
   _.last = function(array, n) {
+    if (n === 1 || n === undefined) {
+      return array[array.length - 1];
+    } else if (n === 0) {
+      return [];
+    } else if (n > 0 && n <= array.length) {
+      return array.slice(array.length - n);
+    } else if (n > array.length) {
+      return array;
+    }
   };
 
   // Call iterator(value, key, collection) for each element of collection.
