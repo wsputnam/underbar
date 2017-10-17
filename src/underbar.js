a06077e8f33584d804bdc2dbf18dcca6adc6b7f8
Your Name <williamputnamjr@gmail.com>
2017-10-17 16:29:57 -0700

    solved for each and filter

--- src/underbar.js
+++ src/underbar.js
@@ -55,6 +55,15 @@
   // Note: _.each does not have a return value, but rather simply runs the
   // iterator function over each item in the input collection.
   _.each = function(collection, iterator) {
+    if (Array.isArray(collection)) {
+      for (var i = 0; i < collection.length; i++) {
+        iterator(collection[i], i, collection);
+      }
+    } else {
+      for (var key in collection) {
+        iterator(collection[key], key, collection);
+      }
+    }
   };
 
   // Returns the index at which value can be found in the array, or -1 if value
@@ -76,6 +85,13 @@
 
   // Return all elements of an array that pass a truth test.
   _.filter = function(collection, test) {
+    var acc = [];
+    for (var i = 0; i < collection.length; i++) {
+      if (test(collection[i]) === true) {
+        acc.push(collection[i]);
+      }
+    }
+    return acc;
   };
 
   // Return all elements of an array that don't pass a truth test.
